62bf25d7dae04c05cd71239e6ca0d6fb83607d51
H4rmonixx <harmonixxgames@gmail.com>
2025-12-03 23:31:38 +0100

    Clear sensitive data from config_smallhost.php
    Removed sensitive database and SMTP configuration details.

--- app/Core/config_smallhost.php
+++ app/Core/config_smallhost.php
@@ -1,18 +1,18 @@
 <?php
 return [
     'db' => [
-        'host' => 'mysql1.small.pl',
-        'name' => 'm3378_ecommerce',
-        'user' => 'm3378_ecommerce',
-        'pass' => '_U2FErQ95f82SQU383xjs"MW&~zFz.'
+        'host' => '',
+        'name' => '',
+        'user' => '',
+        'pass' => ''
     ],
     'phpmailer' => [
-        'smtp_host'        => 'mail1.small.pl',
+        'smtp_host'        => '',
         'smtp_port'        => 587,
         'smtp_secure'      => 'tls',
-        'smtp_username'    => 'harmonixx@harmonixx.smallhost.pl',
-        'smtp_password'    => "iJ0V,2ZRgRuRSmKQGK-9~mJ5d7703'",
-        'mail_from'        => 'harmonixx@harmonixx.smallhost.pl',
-        'mail_from_name'   => 'H.R.M.X'
+        'smtp_username'    => '',
+        'smtp_password'    => "",
+        'mail_from'        => '',
+        'mail_from_name'   => ''
     ]
 ];
